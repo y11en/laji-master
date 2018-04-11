334369ff49ed1afcd768b94e22621325b17fd305
lushiyu <lusy0722@sina.com>
2018-04-11 17:38:03 +0800

    备份

---
+++ src/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import './assets/css/global.css'
+import './assets/css/reset.styl'
+export default {
+  name: 'app'
+}
+</script>
